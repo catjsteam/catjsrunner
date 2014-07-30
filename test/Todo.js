a99328875e4828a84655fa965752dfb2a3521e61
arik <lewin.arik@gmail.com>
2014-07-30 14:56:58 +0300

    minimal test added

--- test/Todo.js
+++ test/Todo.js
@@ -1,12 +1,64 @@
 /**
  * Created by retyk on 16/02/14.
  */
-var assert = require("assert")
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
-    })
-})
+var assert = require("assert");
+
+
+require("./../lib/app.js").run({
+    "run": {
+        "devices": [
+            {
+                "disable": false,
+                "type": "localpc",
+                "runner": {
+                    "name": "chrome",
+                    "address": "/",
+                    "options": {"instances": 4}
+                }
+            },
+            {
+                "disable": false,
+                "type": "localpc",
+                "runner": {
+                    "name": "firefox",
+                    "address": "/",
+                    "options": {"instances": 4}
+                }
+            },
+            {
+                "disable": false,
+                "type": "android",
+                "id": "all",
+                "runner": {
+                    "name": "apk",
+                    "options": {"path" :".lib/resources/catrunner.apk"}
+                }
+            },
+            {
+                "disable": true,
+                "type": "iphone",
+                "id": "all",
+                "runner": {
+                    "name": "agent",
+                    "options": {"ip": "192.168.1.107", "port": "54321", "path": "/cat"}
+                }
+            }
+        ]
+    },
+    "runningEnvironment": {
+        "type": "local|lab",
+        "host": "",
+        "authtoken": ""
+    },
+    "server": {
+        "_host" : "localhost",
+        "host" : "auto",
+        "port" : "8089",
+        "_autostart" : {
+            "args": ["-s"],
+            "nodeStartFile": "catcli.cmd",
+            "_cwd": "/home/user/catgh/CATCore/test/sencha/cat-project",
+            "cwd": "c:/catgithub/CATCore/test/sencha/cat-project"
+        }
+    }
+});
