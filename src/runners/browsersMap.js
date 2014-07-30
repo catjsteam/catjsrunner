d702dd43e185c3c2075f8a511ba2990b61196088
arik <lewin.arik@gmail.com>
2014-07-30 13:42:03 +0300

    new mapper class for retrieving a module of browsers metadata by type

---
+++ src/runners/browsersMap.js
@@ -0,0 +1,31 @@
+module.exports = {
+    
+    darwin: {
+        
+        browser: {
+            chrome: ["Google Chrome"],
+            safari: ["Safari"],
+            firefox: ["Firefox"]
+        }
+        
+    },
+    linux: {
+        
+        browser: {
+            chrome: ["google-chrome"],
+            safari: ["safari"],
+            firefox: ["firefox"]
+        }
+        
+    },
+    win32: {
+        
+        browser: {
+            chrome: ["chrome"],
+            safari: ["safari"],
+            firefox: ["firefox"]
+        }
+        
+    }
+    
+}
